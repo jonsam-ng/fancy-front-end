5bf8ee723a7c25a49c5ea8aa6cdc3eed3ce6394c
jonsam-ng <2824804209@qq.com>
2021-08-03 23:33:03 +0800

    chore: theme for light and dark

--- src/.vuepress/components/Badges.vue
+++ src/.vuepress/components/Badges.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Description: Some description
+ * @Version: 1.0
+ * @Author: jonsam
+ * @Date: 2021-07-30 00:19:39
+ * @LastEditors: jonsam
+ * @LastEditTime: 2021-08-03 22:47:53
+-->
 <template>
   <div class="badge-group">
     <span class="group-tag">标签：</span>
@@ -20,12 +28,12 @@ export default {
 <style scoped>
 .badge-group {
   margin-top: 10px;
-  font-size: 12px;
+  font-size: 10px;
   border-left: 10px solid #44aaab;
   padding-left: 10px;
 }
 .badge-group >>> span {
-  font-size: 12px;
+  font-size: 10px;
 }
 .group-tag {
   line-height: 18px;
